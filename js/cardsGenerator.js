e50d8dcd9b72ac73baf9eb01b2ccd3510694f3fa
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-08-23 12:49:47 -0300

    adding dynamic card

---
+++ js/cardsGenerator.js
@@ -0,0 +1,61 @@
+const cards = document.querySelector("#cards")
+const winesItems = winesData["wines"]
+
+function createCard(id){
+    const card = document.createElement("div")
+    card.setAttribute("id", `card${id}`)
+    card.setAttribute("class", "card card-width p-2")
+        const img = document.createElement("img")
+        img.setAttribute("src",`${winesItems[id]["img"]}`)
+        img.setAttribute("alt",`${winesItems[id]["name"]}`)
+        img.setAttribute("class","card-img-top")
+        const cardBody = document.createElement("div")
+        cardBody.setAttribute("class","card-body")
+            const name = document.createElement("h5")
+            name.setAttribute("class","card-title")
+            const textname = document.createTextNode(`${winesItems[id]["name"]}`)
+            name.appendChild(textname)
+            const description = document.createElement("p")
+            description.setAttribute("class","card-text")
+            const textDescription = document.createTextNode(`${winesItems[id]["description"]}`)
+            description.appendChild(textDescription)
+            const divButtons = document.createElement("div")
+            divButtons.setAttribute("class","row gap-1")
+                const input = document.createElement("input")
+                input.setAttribute("type", "number")
+                input.setAttribute("class", "form-control col")
+                input.setAttribute("placeholder", "0")
+                input.setAttribute("min", "0")
+                input.setAttribute("max", "5")
+                const button = document.createElement("button")
+                button.setAttribute("class","btn btn-primary col")
+                const textButton = document.createTextNode("Agregar")
+                button.appendChild(textButton)
+                const a = document.createElement("a")
+                a.setAttribute("href","#")
+                a.setAttribute("class","btn btn-primary m-1")
+                const textA = document.createTextNode("Más información")
+                a.appendChild(textA)
+
+
+    divButtons.append(input,button,a)
+    cardBody.append(name,description,divButtons)
+    card.append(img,cardBody)
+    cards.appendChild(card)
+}
+
+{
+/* 
+<div class="card card-width">
+    <img src="./img/wines/rotwein-vespa-salento-negroamaro-igt-il-fedale.webp" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Rotwein</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+            the card's content.</p>
+        <div class="row gap-1">
+            <input class="form-control col" type="number" min="0" max="5" placeholder="0">
+            <button class="btn btn-primary col">Agregar</button>
+            <a href="#" class="btn btn-primary m-1">Más información</a>
+        </div>
+    </div>
+</div> */}
---
+++ js/wineData.js
@@ -0,0 +1,86 @@
+const winesData = {
+    "wines": [
+        {
+            "id": 0,
+            "name": "Malbec Reserve",
+            "description": "A full-bodied red wine with rich flavors of blackberry, plum, and hints of chocolate.",
+            "price": 25.99,
+            "year": 2018,
+            "content": 750,
+            "category": "Red",
+            "img": "../img/wines/rotwein-fliederhof-suedtiroler-stmagdalener-classico-doc-gran-marie.webp"
+        },
+        {
+            "id": 1,
+            "name": "Torrontés",
+            "description": "A fragrant white wine with floral aromas and notes of citrus and peach.",
+            "price": 18.5,
+            "year": 2020,
+            "content": 750,
+            "category": "White"
+        },
+        {
+            "id": 2,
+            "name": "Cabernet Sauvignon",
+            "description": "An elegant red wine with a complex bouquet of blackcurrant, tobacco, and spices.",
+            "price": 29.75,
+            "year": 2017,
+            "content": 750,
+            "category": "Red"
+        },
+        {
+            "id": 3,
+            "name": "Chardonnay",
+            "description": "A rich and buttery white wine with flavors of tropical fruits and a hint of vanilla.",
+            "price": 22.0,
+            "year": 2019,
+            "content": 750,
+            "category": "White"
+        },
+        {
+            "id": 4,
+            "name": "Bonarda",
+            "description": "A medium-bodied red wine with notes of ripe berries, black pepper, and a touch of oak.",
+            "price": 19.9,
+            "year": 2018,
+            "content": 750,
+            "category": "Red"
+        },
+        {
+            "id": 5,
+            "name": "Malbec Rosé",
+            "description": "A refreshing rosé wine with delicate flavors of strawberries and a crisp finish.",
+            "price": 16.5,
+            "year": 2020,
+            "content": 750,
+            "category": "Rosé"
+        },
+        {
+            "id": 6,
+            "name": "Syrah",
+            "description": "A bold and spicy red wine with aromas of black fruits, black pepper, and a hint of violet.",
+            "price": 26.5,
+            "year": 2016,
+            "content": 750,
+            "category": "Red"
+        },
+        {
+            "id": 7,
+            "name": "Sauvignon Blanc",
+            "description": "A crisp and vibrant white wine with zesty citrus flavors and a herbal undertone.",
+            "price": 21.75,
+            "year": 2021,
+            "content": 750,
+            "category": "White"
+        },
+        {
+            "id": 8,
+            "name": "Merlot",
+            "description": "A smooth and velvety red wine with flavors of black cherry, plum, and a hint of mocha.",
+            "price": 23.99,
+            "year": 2019,
+            "content": 750,
+            "category": "Red"
+        }
+    ]
+}
