21a42ed15282a1f5fbf22a85592b32c15964bced
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-08-25 21:17:54 -0300

    Working with search filters

--- js/filterSearch.js
+++ js/filterSearch.js
@@ -1,16 +1,43 @@
-const search = document.querySelector("[type='search']");
+const search = document.querySelector("[type='search']")
 
-search.addEventListener("keyup",saveSearchValue);
+search.addEventListener("keyup", saveSearchValue)
+checkboxes.addEventListener("change", searchValues)
+search.addEventListener("keyup", searchValues)
 
-function saveSearchValue(){
-    let searchValue = search.value
+let dynamicCards = document.querySelectorAll("#cards > *")
+
+function saveSearchValue() {
+    let searchValue = search.value.toLowerCase().trim()
     return searchValue
 }
 
-function searchValueInData(){
+function searchValues() {
+    dynamicCards = document.querySelectorAll("#cards > *")
+    for (let i = 0; i < dynamicCards.length; i++) {
+        let name = dynamicCards[i].childNodes[1].childNodes[0].textContent.toLowerCase()
+        let description = dynamicCards[i].childNodes[1].childNodes[1].textContent.toLocaleLowerCase()
+        if (name.includes(saveSearchValue()) || description.includes(saveSearchValue())) {
+            console.log(name)
+            console.log(description)
+        }
+    }
+    console.log(saveSearchValue())
+}
+
+function addCardsWithNameOrDescription() {
     return 0
 }
 
-function addCardWithNameOrDescription(){
-    return 0
-}
+/**
+ Objeto a escanear 
+ dynamicCards
+ 
+ ¿Qué escanear?
+ 
+ Nombre
+ dynamicCards[i].childNodes[1].childNodes[0].textContent
+ 
+ Descripción
+ dynamicCards[i].childNodes[1].childNodes[1].textContent
+ 
+ */
